b09b84c2660b81e2fa64fc7eb5554e20c283e662
Nahuel Correa <nec2multivackergithub@outlook.com>
2022-05-18 21:04:57 -0300

    Implementando contador de lista de deseos

--- src/components/Context/CartContext.jsx
+++ src/components/Context/CartContext.jsx
@@ -2,7 +2,7 @@ import React, { createContext, useState } from 'react';
 export const CartContext = createContext([])
 
 const CartContextProvider = ({ children }) => {
-    const [cartList, setCartList] = useState([])
+    const [cartList, setCartList, wishList, setWishList] = useState([])
     function addToCart(item) {
         const index = cartList.findIndex(i => i.id === item.id)
 
@@ -14,30 +14,48 @@ const CartContextProvider = ({ children }) => {
             setCartList([...cartList, item])
         }
     }
+    function addToWishList(item) {
+        const index = wishList.findIndex(i => i.id === item.id)
+
+        if (index > -1) {
+            const oldItem = wishList[index].quantity
+            wishList.splice(index, 1)
+            setWishList([...wishList, { ...item, quantity: item.quantity + oldItem }])
+        } else {
+            setWishList([...wishList, item])
+        }
+    }
     const removeItem = (id) => {
         const filteredCart = cartList.filter((item) => item.id !== id)
         setCartList(filteredCart)
     }
+    const removeItemWishList = (id) => {
+        const filteredWishList = wishList.filter((item) => item.id !== id)
+        setWishList(filteredWishList)
+    }
     function emptyCart() {
         setCartList([])
+    }
+    function emptyWishList() {
+        setWishList([])
     }
     const cartCounter = () => {
         return (
             cartList.reduce((prev, prod) => (prev + prod.quantity), 0)
         )
     }
-    const totalQuantity = () => {
-        return (
-            cartList.reduce((prev, prod) => (prev + prod.quantity), 0)
-        )
-    }
+    // const wishCounter = () => {
+    //     return (
+    //         wishList.reduce((prev2, prod2) => (prev2 + prod2.quantity), 0)
+    //     )
+    // }
     const totalBuy = () => {
         return (
             cartList.reduce((prev, prod) => (prev + prod.quantity * prod.precio), 0)
         )
     }
     return (
-        <CartContext.Provider value={{ cartList, addToCart, removeItem, emptyCart, cartCounter, totalQuantity, totalBuy }}>
+        <CartContext.Provider value={{ cartList, addToCart, addToWishList, removeItem, removeItemWishList, emptyCart, emptyWishList, cartCounter, totalBuy }}>
             {children}
         </CartContext.Provider>
     )
