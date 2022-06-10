3e7cf7be983ed60ce52baa6a8638c6a837102fa6
Nahuel Correa <nahuelezequielcorrea@gmail.com>
2022-06-10 15:40:47 -0300

    variables del entorno

--- src/index.js
+++ src/index.js
@@ -6,19 +6,19 @@ import reportWebVitals from './reportWebVitals';
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCatYaZMVStsy9QGfqyfBhSdkMBlLGGNwU",
-  authDomain: "wooow-8464a.firebaseapp.com",
-  projectId: "wooow-8464a",
-  storageBucket: "wooow-8464a.appspot.com",
-  messagingSenderId: "86234885968",
-  appId: "1:86234885968:web:fac03e6a12b2654afec655"
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID
 };
 const app = initializeApp(firebaseConfig);
 export const getFireStoreApp = () => {
     return app
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <App />
+root.render( <
+    App / >
 );
 reportWebVitals();
